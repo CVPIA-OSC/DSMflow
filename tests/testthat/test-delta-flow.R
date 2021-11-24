fd24f9bdccdc7ceb6e386a34d147440e595b34ba
alvizek <avizek@flowwest.com>
2021-11-24 12:44:40 -0800

    Adds tests for flow, delta_flow, and bypass_flow objects

--- tests/testthat/test-delta-flow.R
+++ tests/testthat/test-delta-flow.R
@@ -1,8 +1,16 @@
 library(testthat)
 library(tidyverse)
 library(lubridate)
-# DSMflow::delta_flows ---------------------------------------------------------
-test_that("test delta flow structure and values", {
+
+# Tests in this script test:
+# delta_cross_channel_closed - delta structure and values, proportion of days that overtopped
+# delta_flows - delta structure and values, delta_inflow equal delta_flows, delta_flows equals delta_proportion_diverted
+# delta_inflow - delta structure and values, delta_inflow equal delta_flows
+# delta_proportion_diverted - delta_flows equals delta_proportion_diverted, proportion less than 1, delta_total_diverted greater than delta_proportion_diverted, delta structure and values
+# delta_total_diverted - delta_total_diverted greater than delta_proportion_diverted, delta structure and values
+
+# delta structure and values ---------------------------------------------------------
+test_that("delta structure and values", {
   col_names <- c("date", "n_dlt_inflow_cfs", "s_dlt_inflow_cfs", "n_dlt_div_cfs",
                  "s_dlt_div_cfs", "n_dlt_prop_div", "s_dlt_prop_div")
   expect_equal(delta_flows$n_dlt_prop_div, delta_flows$n_dlt_div_cfs/delta_flows$n_dlt_inflow_cfs)
@@ -15,11 +23,15 @@ test_that("test delta flow structure and values", {
   expect_equal(class(delta_flows$n_dlt_prop_div), "numeric")
   expect_equal(class(delta_flows$n_dlt_prop_div), "numeric")
   expect_equal(colnames(delta_flows), col_names)
+  expect_equal(dim(delta_inflow), c(12L, 21L, 2L))
+  expect_equal(dim(delta_proportion_diverted), c(12L, 21L, 2L))
+  expect_equal(dim(delta_total_diverted), c(12L, 21L, 2L))
+  expect_equal(dim(delta_cross_channel_closed), c(2L, 12L))
   # think about testing to check that units seem right (not too small because that would be an indicator of cms)
   # Think about tests to catch outliers
 })
 
-# DSMflow::delta_inflow --------------------------------------------------------
+# delta_inflow equal delta_flows --------------------------------------------------------
 # transform delta_inflow matrix to tidy format
 delta_inflow_df <- map_df(c("North Delta", "South Delta"), function(i) {
   DSMflow::delta_inflow[, , i] %>%
@@ -43,13 +55,12 @@ delta_flows_filtered <- filter(DSMflow::delta_flows, date >= as.Date("1980-01-01
   full_join(delta_inflow_df)
 
 
-test_that("test delta inflow structure and values", {
-  expect_equal(dim(delta_inflow), c(12L, 21L, 2L))
+test_that("delta_inflow equal delta_flows", {
   expect_equal(cfs_to_cms(delta_flows_filtered$n_dlt_inflow_cfs), delta_flows_filtered$n_dlt_inflow_cms)
   expect_equal(cfs_to_cms(delta_flows_filtered$s_dlt_inflow_cfs), delta_flows_filtered$s_dlt_inflow_cms)
 })
 
-# DSMflow::delta_proportion_diverted -------------------------------------------
+# proportion less than 1 -------------------------------------------
 delta_proportion_diverted_df <- map_df(c("North Delta", "South Delta"), function(i) {
   DSMflow::delta_proportion_diverted[, , i] %>%
     as.data.frame() %>%
@@ -71,25 +82,30 @@ delta_flows_filtered <- filter(DSMflow::delta_flows, date >= as.Date("1980-01-01
          month = as.character(month(date, label = T, abbr = T))) %>%
   full_join(delta_proportion_diverted_df)
 
-test_that("Proportion calculated from delta flow and between 0 and 1; delta_flows equals delta_proportion_diverted", {
-  expect_equal(dim(delta_proportion_diverted), c(12L, 21L, 2L))
+test_that("proportion less than 1", {
   expect_true(all(delta_proportion_diverted >= 0))
   expect_true(all(delta_proportion_diverted <= 1))
+
+})
+
+# delta_flows equals delta_proportion_diverted -------------------------------------------
+
+test_that("delta_flows equals delta_proportion_diverted", {
   expect_equal(delta_flows_filtered$n_dlt_prop_div, delta_flows_filtered$n_dlt_proportion_diverted)
   expect_equal(delta_flows_filtered$s_dlt_prop_div, delta_flows_filtered$s_dlt_proportion_diverted)
 })
 
-# DSMflow::delta_total_diverted ------------------------------------------------
-test_that("Total diverted is greater than proportion diverted", {
-  expect_equal(dim(delta_total_diverted), c(12L, 21L, 2L))
+# delta_total_diverted greater than delta_proportion_diverted ------------------------------------------------
+test_that("delta_total_diverted greater than delta_proportion_diverted", {
   expect_true(all(delta_total_diverted > delta_proportion_diverted))
+  expect_true(all(delta_inflow > delta_total_diverted))
 })
 
-# DSMflow::delta_cross_channel_closed ------------------------------------------
-test_that("Proportion of days that overtopped", {
+
+# proportion of days that overtopped ------------------------------------------
+test_that("proportion of days that overtopped", {
   expect_true(all(delta_cross_channel_closed[2, ] >= 0))
   expect_true(all(delta_cross_channel_closed[2, ] <= 1))
   expect_true(all(delta_cross_channel_closed[1, ] <= 31))
   expect_true(all(delta_cross_channel_closed[1, ] >= 0))
-  expect_equal(dim(delta_cross_channel_closed), c(2L, 12L))
 })
