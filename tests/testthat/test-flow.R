f2940d8f1ad2801d9b38988e8ec75893514bd879
liz-fw <lstebbins@flowwest.com>
2022-12-08 15:13:56 -0800

    updates test-flows tests to use lapply

--- tests/testthat/test-flow.R
+++ tests/testthat/test-flow.R
@@ -18,29 +18,22 @@ library(lubridate)
 # flow structure ------------------------------------------------------------
 
 test_that("flow structure",  {
-
-  expect_equal(dim(flows_cfs$biop_2008_2009), c(972, 31))
-  expect_equal(dim(flows_cfs$biop_itp_2018_2019), c(972, 31))
+  expect_equal(unlist(lapply(unname(flows_cfs), function(i) dim(i))), c(972, 31, 972, 31))
   expect_equal(names(flows_cfs), c("biop_2008_2009","biop_itp_2018_2019"))
 
-  expect_equal(dim(freeport_flow$biop_2008_2009), c(12, 21))
-  expect_equal(dim(freeport_flow$biop_itp_2018_2019), c(12, 21))
+  expect_equal(unlist(lapply(unname(freeport_flow), function(i) dim(i))), c(12, 21, 12, 21))
   expect_equal(names(freeport_flow), c("biop_2008_2009","biop_itp_2018_2019"))
 
-  expect_equal(dim(vernalis_flow$biop_2008_2009), c(12, 21))
-  expect_equal(dim(vernalis_flow$biop_itp_2018_2019), c(12, 21))
+  expect_equal(unlist(lapply(unname(vernalis_flow), function(i) dim(i))), c(12, 21, 12, 21))
   expect_equal(names(vernalis_flow), c("biop_2008_2009","biop_itp_2018_2019"))
 
-  expect_equal(dim(wilkins_flow$biop_2008_2009), c(12, 21))
-  expect_equal(dim(wilkins_flow$biop_itp_2018_2019), c(12, 21))
+  expect_equal(unlist(lapply(unname(wilkins_flow), function(i) dim(i))), c(12, 21, 12, 21))
   expect_equal(names(wilkins_flow), c("biop_2008_2009","biop_itp_2018_2019"))
 
-  expect_equal(dim(stockton_flow$biop_2008_2009), c(12, 21))
-  expect_equal(dim(stockton_flow$biop_itp_2018_2019), c(12, 21))
+  expect_equal(unlist(lapply(unname(stockton_flow), function(i) dim(i))), c(12, 21, 12, 21))
   expect_equal(names(stockton_flow), c("biop_2008_2009","biop_itp_2018_2019"))
 
-  expect_equal(dim(upper_sacramento_flows$biop_2008_2009), c(12, 21))
-  expect_equal(dim(upper_sacramento_flows$biop_itp_2018_2019), c(12, 21))
+  expect_equal(unlist(lapply(unname(upper_sacramento_flows), function(i) dim(i))), c(12, 21, 12, 21))
   expect_equal(names(upper_sacramento_flows), c("biop_2008_2009","biop_itp_2018_2019"))
 
 })
@@ -101,19 +94,16 @@ test_that("total_diverted less than mean_flow", {
 
 # proportions less than 1 ------------------------------------------------------
 test_that("proportions less than 1", {
-  expect_true(all(proportion_diverted$biop_2008_2009 >= 0))
-  expect_true(all(proportion_diverted$biop_2008_2009 <= 1))
-  expect_true(all(proportion_flow_natal$biop_2008_2009 >= 0))
-  expect_true(all(proportion_flow_natal$biop_2008_2009 <= 1))
-  expect_true(all(proportion_pulse_flows$biop_2008_2009 >= 0))
-  expect_true(all(proportion_pulse_flows$biop_2008_2009 <= 1))
 
-  expect_true(all(proportion_diverted$biop_itp_2018_2019 >= 0))
-  expect_true(all(proportion_diverted$biop_itp_2018_2019 <= 1))
-  expect_true(all(proportion_flow_natal$biop_itp_2018_2019 >= 0))
-  expect_true(all(proportion_flow_natal$biop_itp_2018_2019 <= 1))
-  expect_true(all(proportion_pulse_flows$biop_itp_2018_2019 >= 0))
-  expect_true(all(proportion_pulse_flows$biop_itp_2018_2019 <= 1))
+  expect_equal(unlist(lapply(unname(proportion_diverted), function(i) all(i >= 0))), c(TRUE, TRUE))
+  expect_equal(unlist(lapply(unname(proportion_diverted), function(i) all(i <= 1))), c(TRUE, TRUE))
+
+  expect_equal(unlist(lapply(unname(proportion_flow_natal), function(i) all(i >= 0))), c(TRUE, TRUE))
+  expect_equal(unlist(lapply(unname(proportion_flow_natal), function(i) all(i <= 1))), c(TRUE, TRUE))
+
+  expect_equal(unlist(lapply(unname(proportion_pulse_flows), function(i) all(i >= 0))), c(TRUE, TRUE))
+  expect_equal(unlist(lapply(unname(proportion_pulse_flows), function(i) all(i <= 1))), c(TRUE, TRUE))
+
 })
 # TODO: some proportions are greater than 1
 # which(proportion_pulse_flows$biop_2008_2009 > 1, arr.ind=T)
